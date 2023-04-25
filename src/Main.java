c1cd46f6ae183850157caf1e1c2f37bb3172a5b0
kislayao <olya.drozd.al@gmail.com>
2023-04-25 18:47:46 +0200

    updates for homework1

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,30 @@ public class Main {
 
         System.out.println("Hello World!");
 
+        // Homework updates
+
+        System.out.print("Please, enter your name: " + "Olga Kislaya");
+        System.out.println();
+        System.out.println("Please, enter your surname: ");
+        System.out.println("Please, enter your age: ");
+
+        System.out.println("Let's calculate sum of 5 + 10: " + (5 + 10));
+        System.out.println("Olga" + " " + "Kislaya" +
+                ". Welcome to my First " +
+                "Lesson to...........");
+
+        // This is an example comment for Git
+        System.out.println();
+        System.out.print("Please, enter your name & surname: ");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        // New Comment
+
+        String name = "test123";
+        int age = 17;
+        double weight = 88.88;
+        char symbol = 'A';
+
     }
 }
