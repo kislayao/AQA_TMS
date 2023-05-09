ee615c0920bb4376665a7aa3553520e80021dfb3
kislayao <olya.drozd.al@gmail.com>
2023-05-09 19:05:04 +0200

    finished task

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,78 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         // Part 1 - Flow Operators
 
         // Task 1 - Program for displaying the name of the time of the year by the number of the month
         // switch-case operator
-    //    getSeasonNameSwitch();
+        getSeasonNameSwitch(new Scanner(System.in));
         System.out.println();
 
         // Task 2 - Program for displaying the name of the time of the year by the number of the month
         // if-else-if operator
-    //    getSeasonNameIf();
+        getSeasonNameIf(new Scanner(System.in));
         System.out.println();
 
         // Task 3 - Program for determining whether entered number is even or odd
-    //    getNumberType();
+        getNumberType(new Scanner(System.in));
         System.out.println();
 
         // Task 4 - Program for determining weather conditions
-    //    getWeatherTemperature();
+        getWeatherTemperature(new Scanner(System.in));
         System.out.println();
 
         // Task 5 - Program for determining the color of the rainbow
-    //    getRainbowColor();
+        getRainbowColor(new Scanner(System.in));
         System.out.println();
 
         // Part 2 - Loops
 
         // Task 1 - Program for getting odd numbers
-    //    getOddNumbers();
+        getOddNumbers();
         System.out.println();
 
         // Task 2 - Program for getting numbers from 5 to 1
-    //    getDecreasingNumbers();
+        getDecreasingNumbers();
         System.out.println();
 
         // Task 3 - Program for getting sum of numbers from1 to entered one
-    //    getSum();
+        getSum(new Scanner(System.in));
         System.out.println();
 
         //Task 4 - Program for getting 7 14 21 ... 98 sequence
-    //    getSequenceOfNumbers7();
+        getSequenceOfNumbers7();
         System.out.println();
 
         // Task 5 - Program for getting 10 first numbers of 0 -5 -10 ... sequence
-    //    getSequenceOfNumbers5();
+        getSequenceOfNumbers5();
         System.out.println();
 
         // Task 6 - Program for getting numbers squared
-    //    getNumbersSquared();
+        getNumbersSquared();
+        System.out.println();
 
         //Additional tasks
 
         // Task 1 - Program for getting 10 first Fibonacci numbers
-    //    getFibonacciNumbers();
+        getFibonacciNumbers();
         System.out.println();
 
         // Task 2 - Program for getting final deposit sum
-    //    getFinalDepositSum();
+        getFinalDepositSum(new Scanner(System.in));
         System.out.println();
 
         // Task 3 - Program for getting multiplication table
         getMultiplicationTable();
+    }
 
+    public static void Scanner(){
+        Scanner scanner = new Scanner(System.in);
         scanner.close();
     }
 
-    public static void getSeasonNameSwitch()
+    public static void getSeasonNameSwitch(Scanner scanner)
     {
         System.out.print("Please, enter month number: ");
         int seasonNumber = scanner.nextInt();
@@ -103,7 +105,7 @@ public class Main {
         }
     }
 
-    public static void getSeasonNameIf()
+    public static void getSeasonNameIf(Scanner scanner)
     {
         System.out.print("Please, enter month number: ");
         int seasonNumber = scanner.nextInt();
@@ -121,7 +123,7 @@ public class Main {
         }
     }
 
-    public static void getNumberType()
+    public static void getNumberType(Scanner scanner)
     {
         System.out.print("Please, enter any number: ");
         int number = scanner.nextInt();
@@ -131,9 +133,10 @@ public class Main {
         } else {
             System.out.print("Entered number is odd");
         }
+        System.out.println();
     }
 
-    public static void getWeatherTemperature()
+    public static void getWeatherTemperature(Scanner scanner)
     {
         System.out.print("Please, enter temperature value: ");
         int temperature = scanner.nextInt();
@@ -147,9 +150,10 @@ public class Main {
         if (temperature <= -20)  {
             System.out.print("The weather is cold");
         }
+        System.out.println();
     }
 
-    public static void getRainbowColor()
+    public static void getRainbowColor(Scanner scanner)
     {
         System.out.print("Please, enter number of color on rainbow: ");
         int rainbowColour = scanner.nextInt();
@@ -182,25 +186,28 @@ public class Main {
         }
     }
 
-
     public static void getOddNumbers()
     {
+        System.out.print("Sequence of odd numbers from 1 to 99: ");
         for (int number = 1; number <= 99; number++)
         {
             System.out.print(number + " ");
             number ++;
         }
+        System.out.println();
     }
 
     public static void getDecreasingNumbers()
     {
+        System.out.print("Decreasing sequence of numbers from 5 to 1: ");
         for (int number = 5; number >= 1; number--)
         {
             System.out.print(number + " ");
         }
+        System.out.println();
     }
 
-    public static void getSum()
+    public static void getSum(Scanner scanner)
     {
         System.out.print("Please, enter any positive number: ");
         int enteredNumber = scanner.nextInt();
@@ -210,37 +217,43 @@ public class Main {
         {
             sum += counter;
         }
-        System.out.println(sum);
+        System.out.println("The sum of numbers from 1 to entered one equals to " + sum);
     }
 
     public static void getSequenceOfNumbers7()
     {
         int number = 7;
 
+        System.out.print("Sequence of numbers with step equals to 7: ");
         while (number <=98){
             System.out.print(number + " ");
             number += 7;
         }
+        System.out.println();
     }
 
     public static void getSequenceOfNumbers5()
     {
         int counter = 1;
         int number = 0;
+        System.out.print("Sequence of numbers with step equals to -5: ");
 
         while (counter <=10){
             System.out.print(number + " ");
             number -= 5;
             counter++;
         }
+        System.out.println();
     }
 
     public static void getNumbersSquared()
     {
+        System.out.print("Sequence of squars of numbers from 10 to 20: ");
         for (int counter = 10; counter <=20; counter++)
         {
             System.out.print(counter*counter + " ");
         }
+        System.out.println();
     }
 
     public static void getFibonacciNumbers()
@@ -248,7 +261,7 @@ public class Main {
         int number1 = 0;
         int number2 = 1;
         int sum;
-        System.out.print("Fibonacci sequence: " + number1 + " " + number2 + " ");
+        System.out.print("10 first numbers of Fibonacci sequence: " + number1 + " " + number2 + " ");
 
         for (int counter = 1; counter <= 8; counter++)
         {
@@ -259,9 +272,10 @@ public class Main {
 
             System.out.print(sum + " ");
         }
+        System.out.println();
     }
 
-    public static void getFinalDepositSum()
+    public static void getFinalDepositSum(Scanner scanner)
     {
         System.out.print("Please, enter initial deposit sum: ");
         float depositSum = scanner.nextFloat();
@@ -273,11 +287,12 @@ public class Main {
         {
             depositSum += depositSum*rate;
         }
-        System.out.println(depositSum);
+        System.out.println("Deposit after " + month + " months will be equals to " + depositSum);
     }
 
     public static void getMultiplicationTable()
     {
+        System.out.println("Multiplication table:");
         for (int counter1 = 1; counter1<=10; counter1++)
         {
             for (int counter2 = 1; counter2<=10; counter2++)
