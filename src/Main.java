235d7c354c8fcca19a36e6ec16fe327a09ebf5e3
kislayao <olya.drozd.al@gmail.com>
2023-05-09 18:24:30 +0200

    finished task

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,289 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        // Part 1 - Flow Operators
+
+        // Task 1 - Program for displaying the name of the time of the year by the number of the month
+        // switch-case operator
+    //    getSeasonNameSwitch();
+        System.out.println();
+
+        // Task 2 - Program for displaying the name of the time of the year by the number of the month
+        // if-else-if operator
+    //    getSeasonNameIf();
+        System.out.println();
+
+        // Task 3 - Program for determining whether entered number is even or odd
+    //    getNumberType();
+        System.out.println();
+
+        // Task 4 - Program for determining weather conditions
+    //    getWeatherTemperature();
+        System.out.println();
+
+        // Task 5 - Program for determining the color of the rainbow
+    //    getRainbowColor();
+        System.out.println();
+
+        // Part 2 - Loops
+
+        // Task 1 - Program for getting odd numbers
+    //    getOddNumbers();
+        System.out.println();
+
+        // Task 2 - Program for getting numbers from 5 to 1
+    //    getDecreasingNumbers();
+        System.out.println();
+
+        // Task 3 - Program for getting sum of numbers from1 to entered one
+    //    getSum();
+        System.out.println();
+
+        //Task 4 - Program for getting 7 14 21 ... 98 sequence
+    //    getSequenceOfNumbers7();
+        System.out.println();
+
+        // Task 5 - Program for getting 10 first numbers of 0 -5 -10 ... sequence
+    //    getSequenceOfNumbers5();
+        System.out.println();
+
+        // Task 6 - Program for getting numbers squared
+    //    getNumbersSquared();
+
+        //Additional tasks
+
+        // Task 1 - Program for getting 10 first Fibonacci numbers
+    //    getFibonacciNumbers();
+        System.out.println();
+
+        // Task 2 - Program for getting final deposit sum
+    //    getFinalDepositSum();
+        System.out.println();
+
+        // Task 3 - Program for getting multiplication table
+        getMultiplicationTable();
+
+        scanner.close();
+    }
+
+    public static void getSeasonNameSwitch()
+    {
+        System.out.print("Please, enter month number: ");
+        int seasonNumber = scanner.nextInt();
+
+        switch(seasonNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Entered number the month corresponds to the time of the year Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Entered number the month corresponds to the time of the year Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Entered number the month corresponds to the time of the year Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Entered number the month corresponds to the time of the year Autumn");
+                break;
+            default:
+                System.out.println("No month corresponds to entered number");
+                break;
+        }
+    }
+
+    public static void getSeasonNameIf()
+    {
+        System.out.print("Please, enter month number: ");
+        int seasonNumber = scanner.nextInt();
+
+        if (seasonNumber == 12 || seasonNumber == 1 || seasonNumber == 2) {
+            System.out.println("Entered number the month corresponds to the time of the year Winter");
+        } else if (seasonNumber == 3 || seasonNumber == 4 || seasonNumber == 5) {
+            System.out.println("Entered number the month corresponds to the time of the year Spring");
+        } else if (seasonNumber == 6 || seasonNumber == 7 || seasonNumber == 8) {
+            System.out.println("Entered number the month corresponds to the time of the year Summer");
+        } else if (seasonNumber == 9 || seasonNumber == 10 || seasonNumber == 11) {
+            System.out.println("Entered number the month corresponds to the time of the year Autumn");
+        } else {
+            System.out.println("No month corresponds to entered number");
+        }
+    }
+
+    public static void getNumberType()
+    {
+        System.out.print("Please, enter any number: ");
+        int number = scanner.nextInt();
+
+        if (number%2 == 0){
+            System.out.print("Entered number is even");
+        } else {
+            System.out.print("Entered number is odd");
+        }
+    }
+
+    public static void getWeatherTemperature()
+    {
+        System.out.print("Please, enter temperature value: ");
+        int temperature = scanner.nextInt();
+
+        if (temperature > -5){
+            System.out.print("The weather is worm");
+        }
+        if (temperature <= -5 && temperature > -20) {
+            System.out.print("The weather is normal");
+        }
+        if (temperature <= -20)  {
+            System.out.print("The weather is cold");
+        }
+    }
+
+    public static void getRainbowColor()
+    {
+        System.out.print("Please, enter number of color on rainbow: ");
+        int rainbowColour = scanner.nextInt();
+
+        switch (rainbowColour){
+            case 1:
+                System.out.println("It's red color");
+                break;
+            case 2:
+                System.out.println("It's orange color");
+                break;
+            case 3:
+                System.out.println("It's yellow color");
+                break;
+            case 4:
+                System.out.println("It's green color");
+                break;
+            case 5:
+                System.out.println("It's blue color");
+                break;
+            case 6:
+                System.out.println("It's indigo color");
+                break;
+            case 7:
+                System.out.println("It's violet color");
+                break;
+            default:
+                System.out.println("There are only 7 colors in rainbow. Please, check entered number");
+                break;
+        }
+    }
 
 
+    public static void getOddNumbers()
+    {
+        for (int number = 1; number <= 99; number++)
+        {
+            System.out.print(number + " ");
+            number ++;
+        }
+    }
+
+    public static void getDecreasingNumbers()
+    {
+        for (int number = 5; number >= 1; number--)
+        {
+            System.out.print(number + " ");
+        }
+    }
+
+    public static void getSum()
+    {
+        System.out.print("Please, enter any positive number: ");
+        int enteredNumber = scanner.nextInt();
+        int sum = 0;
+
+        for (int counter = 1; counter <= enteredNumber; counter++)
+        {
+            sum += counter;
+        }
+        System.out.println(sum);
+    }
+
+    public static void getSequenceOfNumbers7()
+    {
+        int number = 7;
+
+        while (number <=98){
+            System.out.print(number + " ");
+            number += 7;
+        }
+    }
+
+    public static void getSequenceOfNumbers5()
+    {
+        int counter = 1;
+        int number = 0;
+
+        while (counter <=10){
+            System.out.print(number + " ");
+            number -= 5;
+            counter++;
+        }
+    }
+
+    public static void getNumbersSquared()
+    {
+        for (int counter = 10; counter <=20; counter++)
+        {
+            System.out.print(counter*counter + " ");
+        }
+    }
+
+    public static void getFibonacciNumbers()
+    {
+        int number1 = 0;
+        int number2 = 1;
+        int sum;
+        System.out.print("Fibonacci sequence: " + number1 + " " + number2 + " ");
+
+        for (int counter = 1; counter <= 8; counter++)
+        {
+
+            sum = number1 + number2;
+            number1 = number2;
+            number2 = sum;
+
+            System.out.print(sum + " ");
+        }
+    }
+
+    public static void getFinalDepositSum()
+    {
+        System.out.print("Please, enter initial deposit sum: ");
+        float depositSum = scanner.nextFloat();
+        System.out.print("Please, enter amount of months: ");
+        int month = scanner.nextInt();
+        float rate = 0.07f;
+
+        for (int counter = 1; counter <= month; counter++)
+        {
+            depositSum += depositSum*rate;
+        }
+        System.out.println(depositSum);
+    }
+
+    public static void getMultiplicationTable()
+    {
+        for (int counter1 = 1; counter1<=10; counter1++)
+        {
+            for (int counter2 = 1; counter2<=10; counter2++)
+            {
+                System.out.println(counter1 + " * " + counter2 + " = " + counter1*counter2);
+            }
+            System.out.println();
+        }
     }
 }
