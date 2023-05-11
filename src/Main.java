7b2e88ddaa67ccf36395dc7369ded086911b7caf
kislayao <olya.drozd.al@gmail.com>
2023-05-11 14:26:42 +0200

    initial tasks

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,60 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // One dimensional arrays
+
+        // Task 0 - Program for checking whether entered number exists in the array or not
+    //        checkNumberInArray();
+
+        // Task 1 - Program for deleting entered number from array
+        deleteNumberFromArray();
+
 
 
     }
+
+    public static Scanner scanner()
+    {
+        return new Scanner(System.in);
+    }
+
+    public static void checkNumberInArray()
+    {
+        System.out.print("Please, enter number: ");
+        int enteredNumber = scanner().nextInt();
+
+        int[] array = new int[] { 1, 0, 3, 98, 5 };
+
+        for (int index=0; index < array.length; index++)
+        {
+            if (array[index] == enteredNumber){
+                System.out.println("Entered number is in the array");
+                break;
+            }
+            if (index == array.length-1)
+            System.out.println("There is no entered number in the array");
+        }
+    }
+
+    public static void deleteNumberFromArray()
+    {
+        System.out.print("Please, enter number: ");
+        int enteredNumber = scanner().nextInt();
+
+        int[] array = new int[] { 1, 0, 3, 98, 5 };
+        int[] newArray;
+
+        for (int index=0; index < array.length; index++)
+        {
+            if (array[index] == enteredNumber){
+                int [] copy = new int[array.length-1];
+           }
+
+        }
+
+
+    }
+
+
+
 }
